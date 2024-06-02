79ba5875e5e4257e1f6698046f09f33d7db4c002
Rifqi Alghifari <137023042+DomiQQ99@users.noreply.github.com>
2024-06-02 22:20:12 +0700

    new progres

--- TubesDestinationPBO/src/Model/Admin.java
+++ TubesDestinationPBO/src/Model/Admin.java
@@ -5,7 +5,11 @@
  */
 package Model;
 
+import Database.Database;
+import java.sql.SQLException;
+
 public class Admin extends Pengguna {
+
     private String UserName;
     private String Email;
     private String Password;
@@ -41,5 +45,28 @@ public class Admin extends Pengguna {
     public void setVisible(boolean b) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
+    public void addTempatWisata() throws SQLException {
+        TempatWisata wisata = new TempatWisata();
+        Database DataBase = new Database();
+        String sql = "insert into tempatwisata values ('" + wisata.getIdTempat() + "','" + wisata.getNama()
+                + "','" + wisata.getLokasi() + "','" + wisata.getDeskripsi() + "','" + wisata.getRating()
+                + "','" + wisata.getHarga() + "')";
+        DataBase.query(sql);
+    }
+
+    public void deleteTempatWisata() throws SQLException {
+        TempatWisata wisata = new TempatWisata();
+        Database db = new Database();
+        String SQL = "Delete from tempatwisata where nama = ('" + wisata.getNama() + "')";
+        db.query(SQL);
+    }
+
+    public void editTempatWisata(String idtempat, String nama, String lokasi, String deskripsi, String rating, int harga) throws SQLException {
+        Database db = new Database();
+        String sql = "update tempatwisata set id_tempat = '" + idtempat + "',nama = '" + nama
+                + "', lokasi = '" + lokasi + "'deskripsi = '" + deskripsi + "',rating = '" + rating 
+                + "',harga = '" + harga + "';";
+        db.query(sql);
+    }
 }
--- TubesDestinationPBO/src/Model/TempatWisata.java
+++ TubesDestinationPBO/src/Model/TempatWisata.java
@@ -10,5 +10,59 @@ package Model;
  * @author ginas
  */
 public class TempatWisata {
+    private int idTempat;
+    private String nama;
+    private String lokasi;
+    private String deskripsi;
+    private String rating;
+    private int harga;
+
+    public int getIdTempat() {
+        return idTempat;
+    }
+
+    public void setIdTempat(int idTempat) {
+        this.idTempat = idTempat;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getLokasi() {
+        return lokasi;
+    }
+
+    public void setLokasi(String lokasi) {
+        this.lokasi = lokasi;
+    }
+
+    public String getDeskripsi() {
+        return deskripsi;
+    }
+
+    public void setDeskripsi(String deskripsi) {
+        this.deskripsi = deskripsi;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public int getHarga() {
+        return harga;
+    }
+
+    public void setHarga(int harga) {
+        this.harga = harga;
+    }
     
 }
